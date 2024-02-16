6cf07a9ca0f533ddca43752c73e56de8346b45f1
fbansept <bansept.franck@gmail.com>
2024-02-16 11:50:56 +0100

    add jwt login

--- src/main/java/edu/fbansept/demo/controllers/UserController.java
+++ src/main/java/edu/fbansept/demo/controllers/UserController.java
@@ -2,9 +2,13 @@ package edu.fbansept.demo.controllers;
 
 import edu.fbansept.demo.dao.UserDao;
 import edu.fbansept.demo.models.User;
+import edu.fbansept.demo.security.JwtService;
+import edu.fbansept.demo.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +25,12 @@ public class UserController {
     @Autowired
     private BCryptPasswordEncoder encoder;
 
+    @Autowired
+    private AuthenticationProvider authentication;
+
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/sign-in")
     ResponseEntity<User> signIn(@RequestBody User user) {
 
@@ -29,6 +39,25 @@ public class UserController {
 
         return new ResponseEntity<>(user, HttpStatus.CREATED);
 
+    }
+
+    @PostMapping("/login")
+    ResponseEntity<String> login(@RequestBody User user) {
+
+        try {
+            MyUserDetails userDetails = (MyUserDetails)authentication.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getPseudo(),
+                            user.getPassword())
+            ).getPrincipal();
+
+            String jwt = jwtService.getJwtFromUser(userDetails);
+
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
+
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
     }
 
 }
---
+++ src/main/java/edu/fbansept/demo/security/JwtService.java
@@ -0,0 +1,19 @@
+package edu.fbansept.demo.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String getJwtFromUser(MyUserDetails userDetails) {
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .signWith(SignatureAlgorithm.HS256,"secret_du_serveur")
+                .compact();
+
+    }
+
+}
